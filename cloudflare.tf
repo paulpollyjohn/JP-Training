7f7e24bc385350a345dcadec894912f805915edd
paulpollyjohn <166682136+paulpollyjohn@users.noreply.github.com>
2026-02-20 10:42:00 +0800

    first-push

---
+++ cloudflare.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = "XKLo5Vxz5SA8OGfwAsMH4FvS8O8lBy1eacVGrcFt"
+}
+
+variable.zone_id {
+  default = "a6ffd438b97c04648c6b547bcf1b4d88"
+}
+
+# Create a DNS record
+resource "cloudflare_dns_record" "www" {
+  zone_id = var.zone_id
+  name = "www"
+  type = "A"
+  value = "192.168.1.1"
+  ttl = 3600
+  proxied = false
+  comment = "TEST DNS"
+}
